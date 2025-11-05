e66481f314a8a03e8f85cd4dbf90f6a3fd3bbe74
ABASSE <barry96mamoudou@gmail.com>
2025-11-05 16:59:16 +0100

    fix: ajout du port dynamique Render et contrôle image inconnue

--- backend/app.py
+++ backend/app.py
@@ -52,7 +52,7 @@
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
-
+import os
 from flask import Flask, request, jsonify
 import numpy as np
 import tensorflow as tf
@@ -121,4 +121,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
