0fa7cdbb36e1b4c469ee9de1448d6198e81969b8
ABASSE <barry96mamoudou@gmail.com>
2025-11-05 18:58:54 +0100

    changement du port sur app.py

--- backend/app.py
+++ backend/app.py
@@ -122,4 +122,7 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render fournit le port via $PORT
+    app.run(host='0.0.0.0', port=port)
+
